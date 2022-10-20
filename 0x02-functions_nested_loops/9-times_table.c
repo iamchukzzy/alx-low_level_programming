ed9a7c749bdcc19175b715d2adb036e259db6495
Chucks Ozegbe <iamchukzzy@gmail.com>
2022-10-20 07:45:39 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,27 +2,47 @@
 
 /**
  * times_table - print the 9 times table
+ * Return: 0 (Always success)
  */
 void times_table(void)
 {
-	int num, mult, prod;
-	for (num = 0; num <= 9; num++)
+	int a, b, c;
+
+	for (a = 0; a <= 9; a++)
 	{
-		_putchar('0');
-		
-		for (mult = 1; mult <= 9; multi++)
+		for (b = 0; b <= 9; b++)
 		{
-			_putchar(',');
-			_putchar(' ');
+			c = a * b;
+			if ((c / 10) == 0)
+			{
+				if (b == 0)
+				{
+					_putchar('0');
+				}
 
-			prod =  num * mult;
-
-			if (prod <+ 9)
-				_putchar(' ');
+				if (b != 0)
+				{
+					_putchar(' ');
+					_putchar((c % 10) + '0');
+				}
+				if (b < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
 			else
-				_putchar((prod / 10) + '0');
-			_putchar((prod % 10) + '0');
+			{
+				_putchar((c / 10) + '0');
+				_putchar((c % 10) + '0');
+				if (b < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
 		}
+
 		_putchar('\n');
 	}
 }
